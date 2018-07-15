26000b6ecdef6a39bb3ace9e3a74c0e730d19b5d
david roland <david.roland@atticuscreative.co.uk>
2018-07-15 18:18:03 +0100

    created the new hand specialist

---
+++ inc/customizer.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * HandSpecialist Theme Customizer
+ *
+ * @package HandSpecialist
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function handspecialist_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+}
+add_action( 'customize_register', 'handspecialist_customize_register' );
---
+++ inc/widgets.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Register widget area.
+ *
+ * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
+ */
+function handspecialist_widgets_init() {
+	register_sidebar( array(
+		'name'          => esc_html__( 'Sidebar', 'handspecialist' ),
+		'id'            => 'sidebar-1',
+		'description'   => esc_html__( 'Add widgets here.', 'handspecialist' ),
+		'before_widget' => '<section id="%1$s" class="widget card %2$s">',
+		'after_widget'  => '</section>',
+		'before_title'  => '<h3 class="widget-title card-header">',
+		'after_title'   => '</h3>',
+	) );
+}
+add_action( 'widgets_init', 'handspecialist_widgets_init' );
